cf4fc928f82f95c54359b5945a820cc6bbf17c54
thainv <thainvhe153793@fpt.edu.vn>
2022-01-20 08:39:40 +0700

    update homePage

--- src/java/controller/homePage.java
+++ src/java/controller/homePage.java
@@ -56,7 +56,8 @@ public class homePage extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
+        request.getRequestDispatcher("home/homePage.jsp").forward(request, response);
     }
 
     /**
